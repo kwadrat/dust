3722f4c9b775fa9557b33550ed2762f7ea10326f
Piotr Kasprzyk <ciri@ciri.pl>
2018-01-26 11:58:45 +0100

    Make small corrections

--- a.py
+++ a.py
@@ -1,36 +1,46 @@
-#!/usr/bin/python
+#!/usr/bin/env python2
 # -*- coding: UTF-8 -*-
 
-import serial, time, struct, array
+import serial
+import time
+import struct
+import array
 from datetime import datetime
 
+verbose = 0
+
 ser = serial.Serial()
-#ser.port = "/device/000000a2" # Set this to your serial port UNIX
-ser.port = "COM20" # Set this to your serial port
+ser.port = "COM20"
 ser.baudrate = 9600
 ser.open()
 ser.flushInput()
 
-byte, lastbyte = "\x00", "\x00"
-cnt = 0
-while True:
-    lastbyte = byte
-    byte = ser.read(size=1)
-#    print("Got byte %x" %ord(byte))
-    # We got a valid packet header
-    if lastbyte == "\xAA" and byte == "\xC0":
-        sentence = ser.read(size=8) # Read 8 more bytes
-#        print "Sentence size {}".format(len(sentence))
-        readings = struct.unpack('<hhxxcc',sentence) # Decode the packet - big endian, 2 shorts for pm2.5 and pm10, 2 reserved bytes, checksum, message tail
-#        print array.array('B',sentence)
-        pm_25 = readings[0]/10.0
-        pm_10 = readings[1]/10.0
-        # ignoring the checksum and message tail
-        
-        if (cnt == 0 ):
-            line = "PM 2.5: {} ug/m^3  PM 10: {} ug/m^3".format(pm_25, pm_10)
-            print(datetime.now().strftime("%d %b %Y %H:%M:%S.%f: ")+line)
-        cnt += 1
-        if (cnt == 5):
-            cnt = 0
+byte = lastbyte = chr(0)
+line_count = 0
 
+try:
+    while True:
+        lastbyte = byte
+        byte = ser.read(size=1)
+        if verbose:
+            print"Got byte %x" % ord(byte)
+        # We got a valid packet header
+        if lastbyte == chr(170) and byte == chr(192):
+            sentence = ser.read(size=8) # Read 8 more bytes
+            if verbose:
+                print "Sentence size {}".format(len(sentence))
+                print array.array('B', sentence)
+            # Decode the packet - big endian, 2 shorts for pm2.5 and pm10, 2 reserved bytes, checksum, message tail
+            reading_ls = struct.unpack('<hhxxcc', sentence)
+            pm_25 = reading_ls[0] / 10.0
+            pm_10 = reading_ls[1] / 10.0
+            # ignoring the checksum and message tail
+
+            if line_count == 0:
+                line = "PM 2.5: {} ug/m^3  PM 10: {} ug/m^3".format(pm_25, pm_10)
+                print(datetime.now().strftime("%d %b %Y %H:%M:%S.%f: ")+line)
+            line_count += 1
+            if line_count == 5:
+                line_count = 0
+except serial.SerialException:
+    pass
